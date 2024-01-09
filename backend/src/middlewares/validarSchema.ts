f98a5ad6962048f81ae750c18964160056ada1c7
shl0mo <salomao.dacostacruz@gmail.com>
2024-01-09 16:13:21 -0400

    middlewares - validarSchema()

--- backend/src/middlewares/validarSchema.ts
+++ backend/src/middlewares/validarSchema.ts
@@ -1,11 +1,12 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { Schema } from "joi";
 
-function validarSchema (schema: Schema) {
-    return (req: Request, res: Response) => {
+export function validarSchema (schema: Schema) {
+    return (req: Request, res: Response, next: NextFunction) => {
         const { error } = schema.validate(req.body, {
             abortEarly: false
         });
-        if (error) res.status
+        if (error) res.status(422).json(error);
+	else next();
     }
 }
