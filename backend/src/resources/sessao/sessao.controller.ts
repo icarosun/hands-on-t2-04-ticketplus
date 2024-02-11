6355bb1c94a1b3700fa14c7975372909f6b9aa4c
shl0mo <salomao.dacostacruz@gmail.com>
2024-02-11 01:47:03 -0400

    feat: atualiza o controller da sessão
    atualiza o controller da sessão para abranger os dois tipos de usuário

--- backend/src/resources/sessao/sessao.controller.ts
+++ backend/src/resources/sessao/sessao.controller.ts
@@ -1,22 +1,30 @@
 import { Request, Response } from "express";
-import { buscaUsuarioPorEmail } from "../usuario/usuario.service";
+
+import { getCompradorByEmail } from "../comprador/comprador.service";
 
 
 export async function index (req: Request, res: Response) {
     try {
         const nomeUsuario = req.session.nomeUsuario;
-        const sobrenomeUsuario = req.session.sobrenomeUsuario;
         const email = req.session.email;
-        const usuario = await buscaUsuarioPorEmail(email);
-        if (usuario === null) return res.status(308).json({});
-        const saldo = usuario?.saldo;
-        const dadosSessaoUsuario = {
-            nome: nomeUsuario,
-            sobrenome: sobrenomeUsuario,
-            email: email,
-            saldo: saldo
+        if (!nomeUsuario && !email)
+            return res.status(308).json({});
+        const compradorEncontrado = await getCompradorByEmail(email);
+        if (compradorEncontrado) {
+            const saldo = compradorEncontrado.saldo;
+            const dadosSessaoUsuario = {
+                nome: nomeUsuario,
+                email: email,
+                saldo: saldo
+            }
+            return res.status(200).json(dadosSessaoUsuario);
+        } else {
+            const dadosSessaoUsuario = {
+                nome: nomeUsuario,
+                email: email,
+            }
+            return res.status(200).json(dadosSessaoUsuario);
         }
-        return res.status(200).json(dadosSessaoUsuario);
     } catch (error) {
         return res.status(500).json(error);
     }
