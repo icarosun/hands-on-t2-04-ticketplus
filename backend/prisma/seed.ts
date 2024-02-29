13b6539cc763540a9f327999a4700287dd44807b
LGugs <lgpa@icomp.ufam.edu.br>
2024-02-29 13:33:59 -0400

    seed funcional

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-//import { TiposUsuarios } from "../src/resources/tipoUsuario/tipoUsuario.constants";
+
 const prisma = new PrismaClient();
 
 async function comprador() {
@@ -33,6 +33,38 @@ async function organizador() {
   });
 }
 
+async function evento() {
+  await prisma.evento.createMany({
+    data: [
+      {
+        id: 1,
+        titulo: "Chiado da Chinela",
+        descricao:
+          "Forró estilo pé de serra. Mulher não paga. Cerveja liberada a partir da 00:00",
+        localizacao: "Av. Torquato Tapajós, nº 207",
+        faixaEtaria: 18,
+        preco: 12.99,
+        vagas: 100,
+        organizadorId: "19454928-0f2b-44e6-ba87-8e65a1fff621",
+        categoriaEventoId: 1,
+      },
+      {
+        id: 2,
+        titulo: "Suá sem dó",
+        descricao:
+          "Forró ao céu aberto, com piscina e bar liberado até 1h da manhã.",
+        localizacao: "Av. do Turismo, nº 1033",
+        faixaEtaria: 23,
+        preco: 15.89,
+        vagas: 1123,
+        organizadorId: "19454928-0f2b-44e6-ba87-8e65a1fff621",
+        categoriaEventoId: 1,
+      },
+    ],
+    skipDuplicates: true,
+  });
+}
+
 async function categoriaEvento() {
   await prisma.categoriaEvento.createMany({
     data: [
@@ -45,34 +77,82 @@ async function categoriaEvento() {
   });
 }
 
-async function tipoTicket() {
+async function tiposTickets() {
   await prisma.tipoTicket.createMany({
     data: [
       {
         id: 1,
-        descricao: "inteira",
+        descricao: "Inteira",
+        eventoId: 1,
       },
       {
         id: 2,
-        descricao: "meia-entrada",
+        descricao: "Meia-Entrada",
+        eventoId: 1,
       },
       {
         id: 3,
         descricao: "VIP",
+        eventoId: 1,
+      },
+      {
+        id: 4,
+        descricao: "Inteira",
+        eventoId: 2,
+      },
+      {
+        id: 5,
+        descricao: "Meia-Entrada",
+        eventoId: 2,
       },
     ],
     skipDuplicates: true,
   });
 }
 
-tipoTicket()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
+async function tiposTicketsEventos() {
+  await prisma.tiposTicketsEventos.createMany({
+    data: [
+      {
+        tipoTicketId: 1,
+        eventoId: 1,
+        quantidade: 500,
+        preco: 10,
+      },
+      {
+        tipoTicketId: 2,
+        eventoId: 1,
+        quantidade: 500,
+        preco: 5,
+      },
+      {
+        tipoTicketId: 3,
+        eventoId: 1,
+        quantidade: 100,
+        preco: 50,
+      },
+      {
+        tipoTicketId: 4,
+        eventoId: 2,
+        quantidade: 50,
+        preco: 5,
+      },
+      {
+        tipoTicketId: 5,
+        eventoId: 2,
+        quantidade: 100,
+        preco: 2.5,
+      },
+      {
+        tipoTicketId: 6,
+        eventoId: 3,
+        quantidade: 100,
+        preco: 50,
+      },
+    ],
+    skipDuplicates: true,
   });
+}
 
 organizador()
   .catch((e) => {
@@ -90,9 +170,36 @@ categoriaEvento()
   })
   .finally(async () => {
     await prisma.$disconnect();
-});
+  });
+
+evento()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 comprador()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+tiposTickets()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+tiposTicketsEventos()
   .catch((e) => {
     console.error(e);
     process.exit(1);
