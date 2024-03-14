ccb420b7c9715359a2ebf1cdc5b822080fd09e7b
Ícaro <icaropereirasun@gmail.com>
2024-03-13 23:32:50 -0400

    feature: add function navigate to page and test that click

--- frontend/src/components/CardEvento/index.tsx
+++ frontend/src/components/CardEvento/index.tsx
@@ -1,5 +1,8 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import React from "react";
+import { Typography } from '@mui/material';
 import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import { AspectRatio, CardContent, Card } from "@mui/joy";
+import { useNavigate } from "react-router-dom";
 
 interface CardEventoDataType {
   id: number;
@@ -10,34 +13,49 @@ interface CardEventoDataType {
 }
 
 const CardEvento = (props: CardEventoDataType) => {
+  const navigate = useNavigate();
+
+  const handleClick = (id: number) => {
+    navigate(`/evento/${id}`)
+  }
 
   return (
-    <Card sx={{
-      borderRadius: 5,
-      width: 272,
-      height: 340,
-    }}>
-      <CardMedia
-        sx={{ 
-          height: 160,
+    <Card 
+      onClick= {() => handleClick(props.id)}
+      data-testid = "card-evento-component" 
+      variant="plain"
+      sx={{
+        backgroundColor: "white",
+        border: 0,
+        cursor: "pointer",
+        width: 272,
+        height: 340,
+        '&:hover': { boxShadow: 'md', borderColor: 'neutral.outlinedHoverBorder' }
+      }}
+    >
+      <AspectRatio
+        sx = {{
           borderRadius: 5,
-          margin: 2,
-          marginBottom: 0
-      }} image={props.imageUrl}/>
-      <CardContent sx = {{
-        margin: 'auto', 
-        height: "100%" 
-      }}> 
-        <Typography variant = "subtitle1" sx= {{fontWeight: 'bold'}}>
-          {props.title}
-        </Typography>
-        <Typography>
-          {props.description}
-            </Typography>
-        <Typography sx={{textAlign: "center", marginTop: 2}}>
-        <FmdGoodOutlinedIcon />
-          {props.place}
-        </Typography>
+        }}
+      >
+        <img 
+          src = {props.imageUrl}
+          loading="lazy"
+          alt = "Imagem do Evento"
+        />
+      </AspectRatio>
+
+      <CardContent> 
+          <Typography variant = "subtitle1" sx= {{fontWeight: 'bold'}}>
+            {props.title}
+          </Typography>
+          <Typography>
+            {props.description}
+          </Typography>
+          <Typography sx={{textAlign: "center", marginTop: 2}}>
+          <FmdGoodOutlinedIcon />
+            {props.place}
+          </Typography>
       </CardContent>
     </Card>
   );
--- frontend/src/components/tests/CardEvento.test.js
+++ frontend/src/components/tests/CardEvento.test.js
@@ -1,23 +1,47 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CardEvento from "../CardEvento";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Card Evento", () => {
   it("should render the card evento correctly", () => {
     render(
-      <CardEvento 
-        title = "Teste"
-        id='1'
-        imageUrl="https://webacademy.icomp.ufam.edu.br/assets/img/logo_letra_branca.png"
-        description = "Description event"
-        place = "Place" 
-      />
+      <BrowserRouter>
+        <CardEvento 
+          title = "Teste"
+          id='1'
+          imageUrl="https://webacademy.icomp.ufam.edu.br/assets/img/logo_letra_branca.png"
+          description = "Description event"
+          place = "Place" 
+        />
+      </BrowserRouter>
     );
 
     expect(screen.getByRole("heading")).toBeInTheDocument();
     expect(screen.getByRole("img")).toBeInTheDocument();
     expect(screen.getByText("Place")).toBeInTheDocument();
     expect(screen.getByText("Description event")).toBeInTheDocument();
-  })
+    expect(screen.getByTestId("card-evento-component")).toBeInTheDocument();
+  });
+
+  it("should verify the click event on the given DOM", () => {
+    render(
+      <BrowserRouter>
+        <CardEvento 
+          title = "Teste"
+          id='1'
+          imageUrl="https://webacademy.icomp.ufam.edu.br/assets/img/logo_letra_branca.png"
+          description = "Description event"
+          place = "Place" 
+        />
+      </BrowserRouter>
+    );
+
+    const card = screen.getByTestId("card-evento-component");
+    expect(card).toBeInTheDocument();
+
+    fireEvent.click(card);
+
+  });
 })
 
