ef8754498954c758658473525030217c3d7dcff0
LGugs <lgpa@icomp.ufam.edu.br>
2024-03-24 11:00:32 -0400

    fix prisma and migrations

--- backend/prisma/migrations/20240323234654_full_text_serach_titulo_evento/migration.sql
+++ backend/prisma/migrations/20240323234654_full_text_serach_titulo_evento/migration.sql
@@ -10,5 +10,3 @@ ALTER TABLE `eventos` MODIFY `titulo` VARCHAR(191) NOT NULL,
     MODIFY `dataFim` DATETIME NOT NULL,
     MODIFY `dataInicio` DATETIME NOT NULL;
 
--- CreateIndex
-CREATE FULLTEXT INDEX `eventos_titulo_idx` ON `eventos`(`titulo`);
